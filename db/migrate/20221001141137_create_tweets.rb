e5950af7feaa4eaa7cb545268949e4c392b37fa5
Yasir-Zack <rahmanyasir64@gmail.com>
2022-10-01 19:17:36 +0500

    add tweet models and rubocop offense

---
+++ db/migrate/20221001141137_create_tweets.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Tweet models
+
+class CreateTweets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tweets do |t|
+      t.text :tweet
+
+      t.timestamps
+    end
+  end
+end
